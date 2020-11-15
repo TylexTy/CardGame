b3a292d6b277ec9b96394d94555453a0173a0f47
Tyler Bulley <bulleyt@gmail.com>
2020-11-15 01:22:45 -0500

    initial commit

---
+++ deck.js
@@ -0,0 +1,37 @@
+const SUITS = ["♠", "♥", "♣", "♦"]
+const VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+export default class Deck {
+    constructor(cards = FreshDeck()) {
+        this.cards = cards
+    }
+
+    get numberOfCards() {
+        return this.cards.length
+    }
+    shuffle() {
+        for (let i = this.numberOfCards -1; i > 0; i--) {
+            const newIndex = Math.floor(Math.random() * (i + 1))
+            const oldValue = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = oldValue
+        }
+    }
+}
+
+class Card {
+    constructor(suit, value) {
+        this.suit = suit
+        this.value = value
+    }
+}
+
+function FreshDeck() {
+
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit,value)
+        })
+    })
+
+}
